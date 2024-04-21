3ebe03de15d0bd057d265905e70d391f3a1a319f
Jenna Wang <jenna2004@gmail.com>
2024-04-21 04:42:15 -0700

    fixed bugs, added outline

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,7 +1,7 @@
 const Feature = ({ icon, name, description }) => {
     return (
-        <div className="flex flex-col">
-            <div className="flex items-center gap-4 bg-grey px-2 py-2 w-96">
+        <div className="flex flex-col w-1/3">
+            <div className="flex items-center gap-4 bg-grey px-2 py-2">
                 <img src={icon} width={30} />
                 <div className="font-bold"> {name} </div>
             </div>
